08d8286c72d05e3981b2ae1e9622275e37b8e8fa
sangah <kimsangah_@naver.com>
2024-07-04 12:20:19 +0900

    멤버 클래스 1차 작성

--- sns_platform.py
+++ sns_platform.py
@@ -1,10 +1,22 @@
 # ----- 코드 정의 ------
 class Member:
-    # TODO : 코드 구현이 필요합니다.
-
+    def __init__(self, name, username, password):
+        self.name = name
+        self.nusername = username
+        self.password = password
+    
     def display(self):
-        # TODO : 코드 구현이 필요합니다.
-        pass
+        print('이름 :',self.name) 
+        print('아이디 :',self.nusername)
+    
+    # def append(self): 멤버스 리스트에 인스턴스를 넣는 방식을 찾아보고 있습니다ㅠㅠ
+
+a = Member ('홍길동','hong','abc123')
+b = Member ('이순신','lee','abc123')
+c = Member ('박첨지','park','abc123')
+
+a.display() # 메서드확인용
+
 
 
 class Post:
