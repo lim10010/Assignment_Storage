9c71c1cc5d8db9c9a832888ea43811bd13a2e210
lim10010 <lim10010@gmail.com>
2024-07-08 15:19:45 +0900

    과제4 전체구현 완료

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-'''
-가상환경 세팅 후에 터미널에서 아래 두 코드 실행하시면 됩니다!!
-pip install flask
-pip install flask_sqlalchemy
-'''
-
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import random
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sqlalchemy import func
+from itertools import product
+import random
 
 
-#########################################################
-############# 파이썬 코드로 데이터베이스 다루기###################
+
+
+############# 데이터베이스 구현 ##############
 
 # 플라스크와 데이터베이스 연결
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -24,8 +20,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 db = SQLAlchemy(app)
 
 # 테이블 생성 코드
-
-
 class RockPaperSissor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     com = db.Column(db.String(100), nullable=False)
@@ -35,21 +29,57 @@ class RockPaperSissor(db.Model):
     def __rerp__(self):
         return f'사용자 : {self.user} 컴퓨터 : {self.com} 결과 : {self.result}'
 
-
 with app.app_context():
     db.create_all()
 
-#################################################################
-##################################################################
 
 
-@app.route("/")
+############# 코드 구현 ##############
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
+
+    # 초기값 세팅
+    case = ["가위", "바위", "보"]
+    computer = "대기중"
+    player = "대기중"
+    result = None
+
+    if request.method == 'POST':
+        player = request.form['player_choice']
+    else:
+        player = "대기중"
+
+
+    # 가위바위보 승패판정 로직
+    def rsp_result(player,computer):
+        event = (player,computer) # (플레이어,컴퓨타) 듀플로 묶기 -> 밑에 product가 생성한 경우의 수와 비교
+        for x, y in enumerate(product(case, case), 1): # 가위바위보 모든 경우의 수 생성
+            if event == y:
+                if x in [1,5,9]: # 승패판정
+                    return "무"
+                elif x in [3,4,8]:
+                    return "패"
+                else:
+                    return "승"
+
+
+    # player 사용자 선택이 있어야 실행 조건
+    if player != "대기중":
+        computer = str(random.choice(case))
+        result = rsp_result(computer,player)
     
-    ####### 코드 작성 ############
-    
+        # 결과 database에 추가
+        result_history = RockPaperSissor(com=computer, user=player, result=result)
+        db.session.add(result_history)
+        db.session.commit()
+
+
+    # 결과 히스토리 출력
     db_value = db.session.query(RockPaperSissor).all()
 
+
+    # 결과 통계표 출력
     win = db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "승").scalar()
     lost = db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "패").scalar()
     same =  db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "무").scalar()
@@ -58,10 +88,23 @@ def home():
         "win": win,
         "lost":lost,
         "same":same,
+        "com":computer,
+        "user":player,
+        "result":result,
     }
 
     return render_template("game.html", report=db_report, historys=db_value)
 
+
+# 리셋 라우트
+@app.route('/reset', methods=['POST'])
+def reset_database():
+    try:
+        db.drop_all()
+        db.create_all()
+        return redirect(url_for('home'))
+    except Exception as e:
+        return f"예기치 못한 오류 발생: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
