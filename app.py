9ddfef0012fe9a70da8a45e53eb87237486de210
lim10010 <lim10010@gmail.com>
2024-07-08 09:26:51 +0900

    1차 완료

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, render_template, request
 import random
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from sqlalchemy import func
 
 
 #########################################################
@@ -19,20 +19,22 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-            'sqlite:///' + os.path.join(basedir, 'database.db')
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
 
 # 테이블 생성 코드
+
+
 class RockPaperSissor(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     com = db.Column(db.String(100), nullable=False)
     user = db.Column(db.String(100), nullable=False)
     result = db.Column(db.String(100), nullable=False)
 
     def __rerp__(self):
         return f'사용자 : {self.user} 컴퓨터 : {self.com} 결과 : {self.result}'
-    
+
 
 with app.app_context():
     db.create_all()
@@ -40,14 +42,25 @@ with app.app_context():
 #################################################################
 ##################################################################
 
+
 @app.route("/")
 def home():
 
-
     ####### 코드 작성 ############
 
+    win = db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "승").scalar()
+    lost = db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "패").scalar()
+    same =  db.session.query(func.count(RockPaperSissor.result)).filter(RockPaperSissor.result == "무").scalar()
 
-    return render_template("game.html")
+    report = {
+        "win": win,
+        "lost":lost,
+        "same":same,
+    }
+
+
+    return render_template("game.html", data=report)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
