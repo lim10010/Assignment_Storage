ced212bdc07d9099a26f68db33d7b3574e972af5
lim10010 <lim10010@gmail.com>
2024-07-08 15:31:43 +0900

    구현완료 및 업로드

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 # 임시 데이터베이스 역할을 할 리스트 변수
 team_members = []
 
+
 @app.route('/')
 def index():
     return render_template('index.html', team=team_members)
 
 
 # 여행자 정보 입력
-@app.route('/submit-info', methods=['POST'])
-def info_storage():
+ @app.route('/submit-info', methods=['POST'])
+  def info_storage():
     name = request.form.get('name')
     mbti = request.form.get('mbti')
     class_name = request.form.get('class_name')
